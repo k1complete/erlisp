cfdd87307c591cff1f8acdec0d934a1500b1fc1a
k1complete <oda@maqcsa.net>
2023-04-18 15:02:05 +0900

    add next let test

--- test/let_test.erl
+++ test/let_test.erl
@@ -17,7 +17,7 @@ let_test() ->
     {ok, Tokens, _Line} = scan:from_string("(let ((a 1) (b 2)) (+ a b))", Line),
     {ok, Tree} = parser:parse(Tokens),
     C = transpile:form(hd(Tree), []),
-    {value, Result, BindingResult} = erl_eval:expr(erl_syntax:revert(C), []),
+    {value, Result, _BindingResult} = erl_eval:expr(erl_syntax:revert(C), []),
     ?assertEqual(3, Result).
 let_with_tuple_pattern_test() ->
     Line=?LINE,
@@ -25,7 +25,7 @@ let_with_tuple_pattern_test() ->
                                            Line),
     {ok, Tree} = parser:parse(Tokens),
     C = transpile:form(hd(Tree), []),
-    {value, Result, BindingResult} = erl_eval:expr(erl_syntax:revert(C), []),
+    {value, Result, _BindingResult} = erl_eval:expr(erl_syntax:revert(C), []),
     ?assertEqual(6, Result).
 let_with_list_pattern_test() ->
     Line=?LINE,
@@ -33,5 +33,16 @@ let_with_list_pattern_test() ->
                                            Line),
     {ok, Tree} = parser:parse(Tokens),
     C = transpile:form(hd(Tree), []),
-    {value, Result, BindingResult} = erl_eval:expr(erl_syntax:revert(C), []),
+    {value, Result, _BindingResult} = erl_eval:expr(erl_syntax:revert(C), []),
     ?assertEqual(6, Result).
+let_nest_with_list_pattern_test() ->
+    Line=?LINE,
+    A = "(let (((list a b) '(1 2)) (c 3))"++
+        "(+ c (let ((a (+ a b)) (c 10)) (* (* a c) b)))"++
+        ")",
+    {ok, Tokens, _Line} = scan:from_string(A, Line),
+    {ok, Tree} = parser:parse(Tokens),
+    C = transpile:form(hd(Tree), []),
+    {value, Result, _BindingResult} = erl_eval:expr(erl_syntax:revert(C), []),
+    ?assertEqual(63, Result).
+
